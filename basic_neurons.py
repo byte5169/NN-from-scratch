0675b06241fce3d1f289bb04522f8ff61967c800
byte5169 <irshenko@gmail.com>
2021-02-17 12:27:08 +0300

    basic_neurons.py

---
+++ basic_neurons.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+# set seed to reproduse results across runs
+np.random.seed(42)
+
+# by using weights and biases - calculate 3 neurons
+# generate random i, w, b
+inputs = np.random.randint(3, size=5)
+weights = [np.random.rand(3) for i in range(3)]
+biases = np.random.rand(3)
+
+# y = m*x + b, performing calculation
+output1 = [
+    inputs[0] * weights[0][0]
+    + inputs[1] * weights[0][1]
+    + inputs[2] * weights[0][2]
+    + biases[0],
+    inputs[0] * weights[1][0]
+    + inputs[1] * weights[1][1]
+    + inputs[2] * weights[1][2]
+    + biases[1],
+    inputs[0] * weights[2][0]
+    + inputs[1] * weights[2][1]
+    + inputs[2] * weights[2][2]
+    + biases[2],
+]
+print("Results of 1st output: \n", output1)
+
+# the same as above, but using for loops
+output2 = []
+# creating zipped list of weights and biases for each neuron
+for n_w, n_b in zip(weights, biases):
+    n_output = 0
+    # input and weight of neuron
+    for i, w in zip(inputs, n_w):
+        # y = m*x + b
+        n_output += i * w
+    n_output += n_b
+    output2.append(n_output)
+print("Results of 2nd output: \n", output2)
